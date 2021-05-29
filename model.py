967f08790c4b5710934bb1cd9451e16fff95ae98
hani <haniyousfi@gmail.com>
2021-05-29 13:21:11 +0100

    Edit model file

--- model.py
+++ model.py
@@ -1,52 +1,44 @@
-import pytorch
-import pytorch.nn as nn
+import torch.nn as nn
 
 
-class VGG11(nn.Module):
-    def __init__(self, in_channels, num_classes=1000):
-        super(VGG11, self).__init__()
+class VGG(nn.Module):
+    def __init__(self, config, in_channels=3, num_classes=1000):
+        super(VGG, self).__init__()
+        self.config = config
         self.in_channels = in_channels
         self.num_classes = num_classes
+        # select convolutional layer configuration for the VGG net
+        self.covolutional_layers = self.make_conv_layers()
+        self.fully_connected = nn.Sequential(
+            nn.Linear(512*7*7, 4096),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(4096, 4096),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(4096, self.num_classes)
+        )
 
-    def model(self):
-        # convolutional layers
-        self.conv_layers = nn.Sequential(
-            nn.Conv2d(self.in_channels, 64, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(64, 128, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(128, 256, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(256, 512, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(512, 512, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(512, 512, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(512, 512, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2)
-        )
-        # fully connected linear layers
-        self.linear_layers = nn.Sequential(
-            nn.Linear(in_features=512 * 7 * 7, out_features=4096),
-            nn.ReLU(),
-            nn.Dropout2d(0.5),
-            nn.Linear(in_features=4096, out_features=4096),
-            nn.ReLU(),
-            nn.Dropout2d(0.5),
-            nn.Linear(in_features=4096, out_features=self.num_classes)
-        )
+    def make_conv_layers(self):
+        layers = []
+        in_channels = self.in_channels
+        for op in self.config:
+            if op == 'M':
+                layers += [nn.MaxPool2d(kernel_size=2, stride=1)]
+            else:
+                layers += [
+                    nn.Conv2d(in_channels=in_channels,
+                              out_channels=op, kernel_size=3,
+                              padding=1),
+                    nn.BatchNorm2d(op),
+                    nn.ReLU()
+                ]
+                in_channels = op
+        return nn.Sequential(*layers)
 
     def forward(self, x):
-        x = self.conv_layers(x)
+        x = self.convolutional_layers(x)
         # flatten to prepare for the fully connected layers
         x = x.view(x.size(0), -1)
-        x = self.linear_layers(x)
+        x = self.fully_connected(x)
         return x
